40073cd24cb2e2df0592f6306d6f5a6c80e0beea
Yang Zhou <stevenzhou0816100@gmail.com>
2025-05-10 20:54:25 -0400

    runnign

---
+++ launchserver.sh
@@ -0,0 +1,10 @@
+# python -m sglang.launch_server \
+#     --model-path YangZhoumill/scratch_234567_step360 \
+#     --port 30003 \
+#     --host 0.0.0.0   # Explicitly enabling Data Parallelism with 8 GPUs 
+
+python -m sglang.launch_server \
+    --model Qwen/Qwen2.5-1.5B-Instruct \
+    --port 30003 \
+    --dp-size 4 \
+    --host 0.0.0.0 
