9bd96c6b9918ad6a7cfd135d1107a8c28699f3ed
Yang Zhou <stevenzhou0816100@gmail.com>
2025-05-11 21:11:28 -0400

    running

--- LongBench/pred.py
+++ LongBench/pred.py
@@ -2,20 +2,26 @@ import os
 from datasets import load_dataset
 import torch
 import json
-from transformers import AutoTokenizer, LlamaTokenizer, LlamaForCausalLM, AutoModelForCausalLM
+from transformers import AutoTokenizer, LlamaTokenizer, LlamaForCausalLM, AutoModelForCausalLM 
+from openai import OpenAI 
 from tqdm import tqdm
 import numpy as np
 import random
 import argparse
 from llama_flash_attn_monkey_patch import replace_llama_attn_with_flash_attn
 import torch.distributed as dist
-import torch.multiprocessing as mp
+import torch.multiprocessing as mp 
+import time 
 
 def parse_args(args=None):
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--model', type=str, default=None, choices=["llama2-7b-chat-4k", "longchat-v1.5-7b-32k", "xgen-7b-8k", "internlm-7b-8k", "chatglm2-6b", "chatglm2-6b-32k", "chatglm3-6b-32k", "vicuna-v1.5-7b-16k"])
+    parser = argparse.ArgumentParser() 
+    parser.add_argument('--model', type = str, default = None) 
+    # parser.add_argument('--model', type=str, default=None, choices=["llama2-7b-chat-4k", "longchat-v1.5-7b-32k", "xgen-7b-8k", "internlm-7b-8k", "chatglm2-6b", "chatglm2-6b-32k", "chatglm3-6b-32k", "vicuna-v1.5-7b-16k"]) 
     parser.add_argument('--e', action='store_true', help="Evaluate on LongBench-E")
-    return parser.parse_args(args)
+    return parser.parse_args(args) 
+
+URL = "http://127.0.0.1:30003/v1" 
+API_KEY = "token-abc123" 
 
 # This is the customized building prompt for chat models
 def build_chat(tokenizer, prompt, model_name):
@@ -38,8 +44,29 @@ def build_chat(tokenizer, prompt, model_name):
         )
         prompt = header + f" ### Human: {prompt}\n###"
     elif "internlm" in model_name:
-        prompt = f"<|User|>:{prompt}<eoh>\n<|Bot|>:"
-    return prompt
+        prompt = f"<|User|>:{prompt}<eoh>\n<|Bot|>:" 
+    return prompt 
+
+def query_llm(prompt, model, tokenizer, client = None, temperature = 0.5, max_new_tokens = 128, stop = None): 
+    tries = 0 
+    while tries < 5: 
+        tries += 1 
+        try: 
+            completion = client.chat.completions.create(
+                model = model, 
+                messages = [{"role": "user", "content": prompt}], 
+                temperature = temperature, 
+                max_tokens = max_new_tokens, 
+            ) 
+            return completion.choices[0].message.content 
+        except KeyboardInterrupt as e: 
+            raise e 
+        except Exception as e: 
+            print("Error Occurs: \"%s\"        Retry ..."%(str(e))) 
+            time.sleep(1) 
+    else: 
+        print("Max tries. Failed.") 
+        return '' 
 
 def post_process(response, model_name):
     if "xgen" in model_name:
@@ -48,7 +75,11 @@ def post_process(response, model_name):
         response = response.split("<eoa>")[0]
     return response
 
-def get_pred(rank, world_size, data, max_length, max_gen, prompt_format, dataset, device, model_name, model2path, out_path):
+def get_pred(rank, world_size, data, max_length, max_gen, prompt_format, dataset, device, model_name, model2path, out_path): 
+    client = OpenAI(
+        base_url=URL,
+        api_key=API_KEY
+    ) 
     device = torch.device(f'cuda:{rank}')
     model, tokenizer = load_model_and_tokenizer(model2path[model_name], model_name, device)
     for json_obj in tqdm(data):
@@ -60,40 +91,42 @@ def get_pred(rank, world_size, data, max_length, max_gen, prompt_format, dataset
         if len(tokenized_prompt) > max_length:
             half = int(max_length/2)
             prompt = tokenizer.decode(tokenized_prompt[:half], skip_special_tokens=True)+tokenizer.decode(tokenized_prompt[-half:], skip_special_tokens=True)
-        if dataset not in ["trec", "triviaqa", "samsum", "lsht", "lcc", "repobench-p"]: # chat models are better off without build prompts on these tasks
-            prompt = build_chat(tokenizer, prompt, model_name)
-        if "chatglm3" in model_name:
-            if dataset in ["trec", "triviaqa", "samsum", "lsht", "lcc", "repobench-p"]:
-                input = tokenizer(prompt, truncation=False, return_tensors="pt").to(device)
-            else:
-                input = prompt.to(device)
-        else:
-            input = tokenizer(prompt, truncation=False, return_tensors="pt").to(device)
+        # if dataset not in ["trec", "triviaqa", "samsum", "lsht", "lcc", "repobench-p"]: # chat models are better off without build prompts on these tasks 
+            # prompt = build_chat(tokenizer, prompt, model_name) 
+        # if "chatglm3" in model_name:
+        #     if dataset in ["trec", "triviaqa", "samsum", "lsht", "lcc", "repobench-p"]:
+        #         input = tokenizer(prompt, truncation=False, return_tensors="pt").to(device)
+        #     else:
+        #         input = prompt.to(device)
+        # else:
+        #     input = tokenizer(prompt, truncation=False, return_tensors="pt").to(device) 
         context_length = input.input_ids.shape[-1]
         if dataset == "samsum": # prevent illegal output on samsum (model endlessly repeat "\nDialogue"), might be a prompting issue
-            output = model.generate(
-                **input,
-                max_new_tokens=max_gen,
-                num_beams=1,
-                do_sample=False,
-                temperature=1.0,
-                min_length=context_length+1,
-                eos_token_id=[tokenizer.eos_token_id, tokenizer.encode("\n", add_special_tokens=False)[-1]],
-            )[0]
+            # output = model.generate(
+            #     **input,
+            #     max_new_tokens=max_gen,
+            #     num_beams=1,
+            #     do_sample=False,
+            #     temperature=1.0,
+            #     min_length=context_length+1,
+            #     eos_token_id=[tokenizer.eos_token_id, tokenizer.encode("\n", add_special_tokens=False)[-1]],
+            # )[0] 
+            output = query_llm(prompt, model_name, tokenizer, client=client, temperature=0.5, max_new_tokens=max_gen) 
         else:
-            output = model.generate(
-                **input,
-                max_new_tokens=max_gen,
-                num_beams=1,
-                do_sample=False,
-                temperature=1.0,
-            )[0]
-        pred = tokenizer.decode(output[context_length:], skip_special_tokens=True)
+            # output = model.generate(
+            #     **input,
+            #     max_new_tokens=max_gen,
+            #     num_beams=1,
+            #     do_sample=False,
+            #     temperature=1.0,
+            # )[0] 
+            output = query_llm(prompt, model_name, tokenizer, client=client, temperature=0.5, max_new_tokens=max_gen) 
+        # pred = tokenizer.decode(output[context_length:], skip_special_tokens=True) 
+        pred = output[context_length:] 
         pred = post_process(pred, model_name)
         with open(out_path, "a", encoding="utf-8") as f:
             json.dump({"pred": pred, "answers": json_obj["answers"], "all_classes": json_obj["all_classes"], "length": json_obj["length"]}, f, ensure_ascii=False)
-            f.write('\n')
-    dist.destroy_process_group()
+            f.write('\n') 
 
 def seed_everything(seed):
     torch.manual_seed(seed)
@@ -125,9 +158,13 @@ def load_model_and_tokenizer(path, model_name, device):
         )
         model = model.to(device)
         model = model.bfloat16()
-        tokenizer = AutoTokenizer.from_pretrained(path, trust_remote_code=True, use_fast=False)
-    model = model.eval()
-    return model, tokenizer
+        tokenizer = AutoTokenizer.from_pretrained(path, trust_remote_code=True, use_fast=False) 
+    elif "qwen" in model_name: 
+        tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-1.5B-Instruct", trust_remote_code = True) 
+        model = None 
+    if model != None: 
+        model = model.eval() 
+    return model, tokenizer 
 
 if __name__ == '__main__':
     seed_everything(42)
@@ -178,4 +215,4 @@ if __name__ == '__main__':
             p.start()
             processes.append(p)
         for p in processes:
-            p.join()
+            p.join() 
